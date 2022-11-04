de792fc829eea57fbf22b143197eb1a0ce8f53db
Nouah <noahgreatakoni@gmail.com>
2022-11-04 19:37:11 +0100

    useRef added

---
+++ src/components/Modal.js
@@ -0,0 +1,42 @@
+import React,{useRef, useEffect}  from 'react'
+import "./Modal.css"
+const Modal = ({open, children,setOpen,closeOnClick }) => {
+ const dialogRef = useRef();
+ const lastActiveElement = useRef()
+
+ useEffect(() => {
+   if (open) {
+    lastActiveElement.current =document.activeElement
+    dialogRef.current?.showModal();
+   }
+
+   else{
+    dialogRef.current?.close()
+    lastActiveElement.current?.focus()
+   }
+ },[open]);
+
+useEffect(()  => {
+  const dialogNode  = dialogRef.current;
+  const handleCancel = (event) => {
+    event.preventDefault();
+    setOpen(false)
+  };
+  dialogNode.current?.addEventListner("cancel", handleCancel);
+  return  () => dialogNode.removeEventListener("cancel", handleCancel)
+ },[setOpen])
+
+ const  handleCloseOnClick =(event)=> {
+  if(closeOnClick && event.target === dialogRef.current){
+    setOpen(false)
+  }
+
+ };
+  return (
+  <dialog ref={dialogRef} onClick={handleCloseOnClick}>{children}</dialog>
+  );
+ 
+ 
+};
+
+export default Modal
